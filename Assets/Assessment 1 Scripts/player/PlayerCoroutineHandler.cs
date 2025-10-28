2ffbd70540b734661643aa093f4409891106f819
RavenRK <jamesjiang.workemail@gmail.com>
2025-10-28 13:27:32 +0000

    soundBroken

--- Assets/Assessment 1 Scripts/player/PlayerCoroutineHandler.cs
+++ Assets/Assessment 1 Scripts/player/PlayerCoroutineHandler.cs
@@ -6,6 +6,7 @@ public class PlayerCoroutineHandler : MonoBehaviour
     public bool bCanDebug = false;
     public playerCharacter PC { get; private set; }
     public playerStateManager StateManager { get; private set; }
+    public PlayerSoundManager SoundManager { get; private set; }
 
     public Coroutine C_GroundCheck { get; private set; } // update
     public Coroutine C_VerticalDirectionCheck { get; private set; }
@@ -13,11 +14,13 @@ public class PlayerCoroutineHandler : MonoBehaviour
     public Coroutine C_CoyoteTimeCheck { get; private set; }
     public Coroutine C_MoveCheck { get; private set; }
     public Coroutine C_JumpApexCheck { get; private set; }
+    public Coroutine C_MoveSoundCheck { get; private set; }
 
     public void Awake()
     {
         PC = GetComponent<playerCharacter>();
         StateManager = GetComponent<playerStateManager>();
+        SoundManager = GetComponentInChildren<PlayerSoundManager>();
     }
     public IEnumerator GroundCheckUpdate()
     {
@@ -28,34 +31,10 @@ public class PlayerCoroutineHandler : MonoBehaviour
         }
         if (bCanDebug) Log.Green("Grounded Detected");
         PC.originalValuesReset();
+        SoundManager.PlayLandSound();
         StateManager.ChangeState(StateManager.IdleState);
         C_GroundCheck = null;
     }
-    public IEnumerator JumpApexUpdate()
-    {
-        while (PC.rb2D.linearVelocity.y > 1)
-        {
-            yield return null;
-        }
-        StateManager.ChangeState(StateManager.JumpApex);
-        C_JumpApexCheck = null;
-    }
-    // check of we are falling
-    // Input buffer
-    // timer for when to move
-    #region VerticalDirectionCheck
-    public IEnumerator VerticalDirectionCheck()
-    {
-        while (PC.rb2D.linearVelocity.y > -0.5)
-        {
-            yield return null;
-        }
-        StateManager.ChangeState(StateManager.AirState);
-        yield return new WaitForEndOfFrame();
-        C_VerticalDirectionCheck = null;
-    }
-    #endregion
-    #region Input Buffer Update
     public IEnumerator InputBufferUpdate()
     {
         PC.bIsInputbuffer = true;
@@ -73,12 +52,29 @@ public class PlayerCoroutineHandler : MonoBehaviour
             yield return null;
 
             if (PC.jumpBufferTimer <= 0)
-            break;
+                break;
         }
         C_InputBufferCheck = null;
     }
-    #endregion
-    #region CoyoteTimeUpdate
+    public IEnumerator JumpApexUpdate()
+    {
+        while (PC.rb2D.linearVelocity.y > 1)
+        {
+            yield return null;
+        }
+        StateManager.ChangeState(StateManager.JumpApex);
+        C_JumpApexCheck = null;
+    }
+    public IEnumerator VerticalDirectionCheck()
+    {
+        while (PC.rb2D.linearVelocity.y > -0.5)
+        {
+            yield return null;
+        }
+        StateManager.ChangeState(StateManager.AirState);
+        yield return new WaitForEndOfFrame();
+        C_VerticalDirectionCheck = null;
+    }
     public IEnumerator CoyoteTimeUpdate()
     {
         if (bCanDebug) Log.Red("coyote timer start");
@@ -93,21 +89,27 @@ public class PlayerCoroutineHandler : MonoBehaviour
         PC.bCanCoyoteJump = false;
         C_CoyoteTimeCheck = null;
     }
-    #endregion
-    #region MoveUpdate
     public IEnumerator MoveUpdate()
     {
         while (PC.Movedirection != Vector2.zero)
         {
             yield return null;
             PC.rb2D.linearVelocity = new Vector2(PC.Movedirection.x * PC.moveSpeed, PC.rb2D.linearVelocity.y);
+            RunCoroutine(MoveSoundTimer(), C_MoveSoundCheck);
         }
         StateManager.ChangeState(StateManager.IdleState);
     }
-    #endregion
-
+    public IEnumerator MoveSoundTimer()
+    {
+        while (PC.Movedirection != Vector2.zero)
+        {
+            yield return new WaitForSeconds(.25f);
+            SoundManager.PlayMoveSound();
+        }
+        C_MoveSoundCheck = null;
+    }
     // << start & stop base funcs
-    #region Start / Stop Coroutines Func
+        #region Start / Stop Coroutines Func
     public Coroutine RunCoroutine(IEnumerator IEnum, Coroutine C_coroutine)
     {
         if (C_coroutine == null)
